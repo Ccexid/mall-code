ea3b1a9d865e42a14b8e2bd34eb7d064d6b6316b
zhangxb <863232387@qq.com>
2025-08-22 17:49:44 +0800

    feat(core): 添加 MyBatis Flex 自定义配置
    - 新增 MyBatisFlexConfiguration 类实现 MyBatisFlexCustomizer 接口- 自定义 Flex 全局配置，开启审计功能并设置消息收集器
    - 打印 SQL审计日志，包括完整 SQL 语句和执行时间

---
+++ mall-framework/mall-core/src/main/java/com/ccexid/core/configuration/MyBatisFlexConfiguration.java
@@ -0,0 +1,25 @@
+package com.ccexid.core.configuration;
+
+import com.mybatisflex.core.FlexGlobalConfig;
+import com.mybatisflex.core.audit.AuditManager;
+import com.mybatisflex.spring.boot.MyBatisFlexCustomizer;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class MyBatisFlexConfiguration implements MyBatisFlexCustomizer {
+        /**
+     * 自定义Flex全局配置
+     *
+     * @param flexGlobalConfig Flex全局配置对象，用于配置Flex相关的全局参数
+     */
+    @Override
+    public void customize(FlexGlobalConfig flexGlobalConfig) {
+        log.info("MyBatisFlexConfiguration.customize");
+        // 开启审计功能
+        AuditManager.setAuditEnable(true);
+        AuditManager.setMessageCollector(auditMessage -> {
+            log.info("{},{}ms", auditMessage.getFullSql(), auditMessage.getElapsedTime());
+        });
+    }
+
+}
