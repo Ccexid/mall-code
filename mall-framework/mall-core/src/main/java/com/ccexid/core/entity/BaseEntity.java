633d141f3c252f54f0a439f4268eb0da4e4eb50e
zhangxb <863232387@qq.com>
2025-08-22 13:59:56 +0800

    feat(core): 添加 BaseEntity 基类
    - 新增 BaseEntity 类，继承自 Model<T>，实现 Serializable 接口
    - 添加通用的 id、createTime 和 updateTime 字段- 使用 Lombok 注解简化代码，提高可读性和易维护性
    - 通过 MyBatis Flex 注解配置主键和时间字段的自动更新

---
+++ mall-framework/mall-core/src/main/java/com/ccexid/core/entity/BaseEntity.java
@@ -0,0 +1,30 @@
+package com.ccexid.core.entity;
+
+import com.mybatisflex.annotation.Column;
+import com.mybatisflex.annotation.Id;
+import com.mybatisflex.annotation.KeyType;
+import com.mybatisflex.core.activerecord.Model;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.experimental.Accessors;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.Date;
+
+@EqualsAndHashCode(callSuper = true)
+@Accessors(chain = true)
+@Data(staticConstructor = "create")
+public class BaseEntity<T extends Model<T>> extends Model<T> implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @Id(keyType = KeyType.Auto)
+    private Long id;
+
+    @Column(onInsertValue = "now()")
+    private Date createTime;
+
+    @Column(onUpdateValue = "now()", onInsertValue = "now()")
+    private Date updateTime;
+}
