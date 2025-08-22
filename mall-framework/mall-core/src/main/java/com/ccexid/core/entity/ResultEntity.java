54566d4d3c3a47e697aa3b300cb99a36dc67e297
zhangxb <863232387@qq.com>
2025-08-22 09:58:04 +0800

    refactor(core): 添加基础模型接口和结果实体的文档注释- 在 BaseModel 接口上添加了基础模型接口的文档注释 - 在 ResultEntity 类上添加了返回结果实体的文档注释，包括参数说明

--- mall-framework/mall-core/src/main/java/com/ccexid/core/entity/ResultEntity.java
+++ mall-framework/mall-core/src/main/java/com/ccexid/core/entity/ResultEntity.java
@@ -4,6 +4,14 @@ import com.ccexid.core.model.BaseModel;
 
 import java.io.Serial;
 
+/**
+ * 返回结果实体
+ *
+ * @param code    状态码
+ * @param message 提示信息
+ * @param data    数据
+ * @param <T>     数据类型
+ */
 public record ResultEntity<T>(
         int code,
         String message,
--- mall-framework/mall-core/src/main/java/com/ccexid/core/model/BaseModel.java
+++ mall-framework/mall-core/src/main/java/com/ccexid/core/model/BaseModel.java
@@ -2,5 +2,8 @@ package com.ccexid.core.model;
 
 import java.io.Serializable;
 
+/**
+ * 基础模型接口
+ */
 public interface BaseModel extends Serializable {
 }
