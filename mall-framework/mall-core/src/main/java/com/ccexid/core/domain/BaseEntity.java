545e8524aa65f89532d3f79e3a2fc77479e1eee5
zhangxb <863232387@qq.com>
2025-09-03 09:00:15 +0800

    refactor(core): 移除 BaseEntity 中的 id 字段并添加 Kv 类
    - 从 BaseEntity 类中删除了 @Id 注解和 id 字段
    - 新增 Kv 类，用于创建键值对对象，支持链式调用和各种操作

--- mall-framework/mall-core/src/main/java/com/ccexid/core/domain/BaseEntity.java
+++ mall-framework/mall-core/src/main/java/com/ccexid/core/domain/BaseEntity.java
@@ -1,8 +1,6 @@
 package com.ccexid.core.domain;
 
 import com.mybatisflex.annotation.Column;
-import com.mybatisflex.annotation.Id;
-import com.mybatisflex.annotation.KeyType;
 import com.mybatisflex.core.activerecord.Model;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -18,9 +16,6 @@ import java.util.Date;
 public class BaseEntity<T extends Model<T>> extends Model<T> implements Serializable {
     @Serial
     private static final long serialVersionUID = 1L;
-
-    @Id(keyType = KeyType.Auto)
-    private Long id;
 
     @Column(onInsertValue = "now()")
     private Date createTime;
---
+++ mall-framework/mall-core/src/main/java/com/ccexid/core/support/Kv.java
@@ -0,0 +1,97 @@
+package com.ccexid.core.support;
+
+import org.springframework.util.LinkedCaseInsensitiveMap;
+
+import java.util.Map;
+
+public class Kv<T> extends LinkedCaseInsensitiveMap<T> {
+    private Kv() {
+    }
+
+    /**
+     * 创建一个新的Kv实例
+     *
+     * @param <T> 泛型类型参数，表示Kv中存储的值的类型
+     * @return 返回新创建的Kv实例
+     */
+    public static <T> Kv<T> create() {
+        return new Kv<>();
+    }
+
+    /**
+     * 设置键值对
+     *
+     * @param key   键名
+     * @param value 值
+     * @return 返回当前Kv对象，支持链式调用
+     */
+    public Kv<T> set(String key, T value) {
+        put(key, value);
+        return this;
+    }
+
+    /**
+     * 当值不为null时，设置键值对
+     *
+     * @param key   键名
+     * @param value 值对象，当该值不为null时才会被设置
+     * @return 返回当前Kv对象，支持链式调用
+     */
+    public Kv<T> setIfNotNull(String key, T value) {
+        // 当值不为null时才进行设置操作
+        if (value != null && key != null) {
+            put(key, value);
+        }
+        return this;
+    }
+
+
+    /**
+     * 获取指定键名的值
+     *
+     * @param attr         键名
+     * @param defaultValue 默认值s
+     * @return 返回指定键名的值，如果键名不存在则返回默认值
+     */
+    @SuppressWarnings("unchecked")
+    public T get(String attr, T defaultValue) {
+        Object result = this.get(attr);
+        return (T) (result != null ? result : defaultValue);
+    }
+
+
+    /**
+     * 将指定映射中的所有键值对添加到当前Kv对象中
+     *
+     * @param map 要添加的映射，键为String类型，值为T类型，可以为null
+     * @return 返回当前Kv对象实例，支持链式调用
+     */
+    public Kv<T> setAll(Map<? extends String, ? extends T> map) {
+        // 如果传入的映射不为null，则将其所有键值对添加到当前对象中
+        if (map != null) {
+            this.putAll(map);
+        }
+        return this;
+    }
+
+    /**
+     * 创建当前Kv对象的副本
+     *
+     * @return 返回一个新的Kv对象，包含与当前对象相同的所有键值对
+     */
+    @Override
+    public Kv<T> clone() {
+        // 创建新的Kv实例并复制所有键值对
+        Kv<T> clone = new Kv<>();
+        try {
+            clone.putAll(this);
+        } catch (Exception e) {
+            // 如果克隆过程中出现异常，返回空的Kv实例而不是null
+            return new Kv<>();
+        }
+        return clone;
+    }
+
+
+}
+
