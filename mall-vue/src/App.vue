cacb24e5f8be7b2b1a23fc8cdb3a07c5486958ea
zhangxb <863232387@qq.com>
2025-09-05 13:50:58 +0800

    初始化项目

--- mall-vue/src/App.vue
+++ mall-vue/src/App.vue
@@ -1,11 +1,20 @@
-<script setup lang="ts"></script>
-
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <el-tabs v-model="activeTab">
+    <tab-pane label="用户手册" :key-value="1" icon="StarFilled">1111</tab-pane>
+    <tab-pane label="实用联系人" :key-value="2" icon="StarFilled">2222</tab-pane>
+    <tab-pane label="会议纪要" :key-value="3" icon="StarFilled">3333</tab-pane>
+    <tab-pane label="管理处联系人" :key-value="4" icon="StarFilled">4444</tab-pane>
+    <tab-pane label="房屋规则" :key-value="5" icon="StarFilled">5555</tab-pane>
+    <tab-pane label="在线表格" :key-value="6" icon="StarFilled">6666</tab-pane>
+    <tab-pane label="装修标准" :key-value="7" icon="StarFilled">7777</tab-pane>
+  </el-tabs>
 </template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+import TabPane from '@/components/TabPane/index.vue'
+
+const activeTab = ref(1)
+</script>
 
 <style scoped></style>
--- mall-vue/src/main.ts
+++ mall-vue/src/main.ts
@@ -1,12 +1,18 @@
-import { createApp } from 'vue'
+import { createApp, createVNode } from 'vue'
 import { createPinia } from 'pinia'
-
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
 import App from './App.vue'
 import router from './router'
+import * as Icons from "@element-plus/icons-vue";
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 
 const app = createApp(App)
-
+app.use(ElementPlus)
 app.use(createPinia())
 app.use(router)
-
+// 注册所有图标
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+  app.component(key, component)
+}
 app.mount('#app')
