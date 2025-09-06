9885ab82a028f1aa560434db1f7c4acf2fa395b1
zhangxb <863232387@qq.com>
2025-09-06 22:34:16 +0800

    更新代码

--- mall-vue/env.d.ts
+++ mall-vue/env.d.ts
@@ -1,1 +1,9 @@
 /// <reference types="vite/client" />
+
+// 声明 Vue 单文件组件的类型
+declare module '*.vue' {
+  import type { DefineComponent } from 'vue'
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const component: DefineComponent<Record<string, any>, Record<string, any>, any>
+  export default component
+}
