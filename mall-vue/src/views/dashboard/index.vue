9a1da4108af7e45be33b185d724fef389a5efb1a
zhangxb <863232387@qq.com>
2025-09-08 09:31:51 +0800

    提交变更代码，针对代码进按需分配

--- mall-vue/src/views/dashboard/index.vue
+++ mall-vue/src/views/dashboard/index.vue
@@ -1,10 +1,17 @@
 <template>
   <div class="dashboard">
-    111
+    <el-row :gutter="20">
+      121
+      <el-icon>
+        <Edit />
+      </el-icon>
+    </el-row>
   </div>
 </template>
 
 <script setup lang="ts">
+import { Edit } from '@element-plus/icons-vue'
+
 defineOptions({
   name: "DashboardViews",
 })
--- mall-vue/vite.config.ts
+++ mall-vue/vite.config.ts
@@ -4,10 +4,50 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import tailwindcss from '@tailwindcss/vite'
+import Icons from 'unplugin-icons/vite'
+import IconsResolver from 'unplugin-icons/resolver'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), tailwindcss(), vueDevTools()],
+  plugins: [
+    vue(),
+    tailwindcss(),
+    vueDevTools(),
+    AutoImport({
+      // 自动导入 Vue 相关函数，如：ref, reactive, toRef 等
+      imports: ['vue'],
+      // 自动导入 Element Plus 相关函数，如：ElMessage, ElMessageBox... (带样式)
+      resolvers: [
+        ElementPlusResolver(),
+
+        // Auto import icon components
+        // 自动导入图标组件
+        IconsResolver({
+          prefix: 'Icon',
+        }),
+      ],
+      dts: fileURLToPath(new URL('./src/auto-imports.ts', import.meta.url)),
+    }),
+    Components({
+      resolvers: [
+        // Auto register icon components
+        // 自动注册图标组件
+        IconsResolver({
+          enabledCollections: ['ep'],
+        }),
+        // Auto register Element Plus components
+        // 自动导入 Element Plus 组件
+        ElementPlusResolver(),
+      ],
+      dts: fileURLToPath(new URL('./src/components.d.ts', import.meta.url)),
+    }),
+    Icons({
+      autoInstall: true,
+    }),
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
